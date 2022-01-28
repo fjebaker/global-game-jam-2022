7e1244a04d7aeae8e09eaa4d46653fbc261f5f27
John Wiggins <jwiggins@users.noreply.github.com>
2022-01-28 23:35:42 +0100

    Add flag art (#16)

--- src/screens/art-sandbox.zig
+++ src/screens/art-sandbox.zig
@@ -1,11 +1,14 @@
 const w4 = @import("../wasm4.zig");
 const boris = @import("../assets/boris.zig");
+const flag = @import("../assets/flag.zig");
 
 var art_ticks: u32 = 0;
 
 pub fn update() void {
-    const xPos: i32 = 72;
-    const yPos: i32 = 72;
+    const bxPos: i32 = 72;
+    const byPos: i32 = 72;
+    const fxPos: i32 = 90;
+    const fyPos: i32 = 72;
 
     var xOff: u32 = 0;
     if (art_ticks % 20 > 10) {
@@ -15,11 +18,21 @@ pub fn update() void {
     w4.DRAW_COLORS.* = 0x4320;
     w4.blitSub(
         &boris.data,
-        xPos, yPos, // x, y
+        bxPos, byPos, // x, y
         boris.height, boris.height, // w, h; Assumes square
         xOff, 0, // src_x, src_y
         boris.width, // Assumes stride and width are equal
         boris.flags
+    );
+
+     w4.DRAW_COLORS.* = 0x2430;
+    w4.blitSub(
+        &flag.data,
+        fxPos, fyPos, // x, y
+        flag.height, flag.height, // w, h; Assumes square
+        xOff, 0, // src_x, src_y
+        flag.width, // Assumes stride and width are equal
+        flag.flags
     );
 
     art_ticks += 1;
