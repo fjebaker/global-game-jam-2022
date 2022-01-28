64b63428eb7e01498eab8338cce6019d152de112
fjebaker <fergusbkr@gmail.com>
2022-01-28 23:05:50 +0000

    draw party

--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,13 @@
 const state = @import("state-machine.zig");
 const mainmenu = @import("screens/main-menu.zig");
+const party = @import("screens/party.zig");
 
-var game_state: state.State = state.State.IN_MENU;
+var game_state: state.State = state.State.AT_PARTY;
 
 export fn update() void {
     switch (game_state) {
         .IN_MENU => mainmenu.drawMenu(),
+        .AT_PARTY => party.doround(),
         else => {}
     }
 }
