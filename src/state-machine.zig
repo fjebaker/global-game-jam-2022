3a6090a901b0fbce22278ce5f1262be17420134c
fjebaker <fergusbkr@gmail.com>
2022-01-28 20:58:39 +0000

    empty state machine

---
+++ src/state-machine.zig
@@ -0,0 +1,10 @@
+// add global variables representing what state the game is in
+pub const State = enum {
+    IN_MENU,
+    AT_PARTY,
+    AT_PRESS_CONFERENCE,
+    AT_HOUSE_OF_COMMONS,
+    SUE_GRAY,
+    PAUSED_SCREEN
+};
+
