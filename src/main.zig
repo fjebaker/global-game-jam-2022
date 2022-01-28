88fe3cfa234d011cff66526e3d9610db55c47545
fjebaker <fergusbkr@gmail.com>
2022-01-28 20:58:50 +0000

    change hello world to use state machine

--- src/main.zig
+++ src/main.zig
@@ -1,25 +1,11 @@
-const w4 = @import("wasm4.zig");
+const state = @import("state-machine.zig");
+const mainmenu = @import("screens/main-menu.zig");
 
-const smiley = [8]u8{
-    0b11000011,
-    0b10000001,
-    0b00100100,
-    0b00100100,
-    0b00000000,
-    0b00100100,
-    0b10011001,
-    0b11000011,
-};
+var game_state: state.State = state.State.IN_MENU;
 
 export fn update() void {
-    w4.DRAW_COLORS.* = 2;
-    w4.text("Hello from Zig!", 10, 10);
-
-    const gamepad = w4.GAMEPAD1.*;
-    if (gamepad & w4.BUTTON_1 != 0) {
-        w4.DRAW_COLORS.* = 4;
+    switch (game_state) {
+        .IN_MENU => mainmenu.drawMenu(),
+        else => {}
     }
-
-    w4.blit(&smiley, 76, 76, 8, 8, w4.BLIT_1BPP);
-    w4.text("Press X to blink", 16, 90);
 }
