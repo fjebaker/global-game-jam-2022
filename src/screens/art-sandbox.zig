2812510b3ccfa07a5687668e469e13d3f6a481cf
John Wiggins <jwiggins@users.noreply.github.com>
2022-01-29 00:38:38 +0100

    Add press asset (#25)

--- src/screens/art-sandbox.zig
+++ src/screens/art-sandbox.zig
@@ -1,15 +1,11 @@
 const w4 = @import("../wasm4.zig");
 const boris = @import("../assets/boris.zig");
 const flag = @import("../assets/flag.zig");
+const press = @import("../assets/press.zig");
 
 var art_ticks: u32 = 0;
 
 pub fn update() void {
-    const bxPos: i32 = 72;
-    const byPos: i32 = 72;
-    const fxPos: i32 = 90;
-    const fyPos: i32 = 72;
-
     var xOff: u32 = 0;
     if (art_ticks % 20 > 10) {
         xOff = 16;
@@ -18,22 +14,33 @@ pub fn update() void {
     w4.DRAW_COLORS.* = 0x4320;
     w4.blitSub(
         &boris.data,
-        bxPos, byPos, // x, y
+        72, 72, // x, y
         boris.height, boris.height, // w, h; Assumes square
         xOff, 0, // src_x, src_y
         boris.width, // Assumes stride and width are equal
         boris.flags
     );
 
-     w4.DRAW_COLORS.* = 0x2430;
+    w4.DRAW_COLORS.* = 0x2430;
     w4.blitSub(
         &flag.data,
-        fxPos, fyPos, // x, y
+        90, 72, // x, y
         flag.height, flag.height, // w, h; Assumes square
         xOff, 0, // src_x, src_y
         flag.width, // Assumes stride and width are equal
         flag.flags
     );
+
+    for ([_]i32{1, 2, 3, 4}) |i| {
+        w4.blitSub(
+            &press.data,
+            30 + i * 20, 40, // x, y
+            press.height, press.height, // w, h; Assumes square
+            xOff, 0, // src_x, src_y
+            press.width, // Assumes stride and width are equal
+            press.flags
+        );
+    }
 
     art_ticks += 1;
 }
