e7d51fe01404524d47a9add0fabe5bf220feba0e
OneSlightWeirdo <www.aereregion.webs.com@live.co.uk>
2022-01-28 23:01:19 +0000

    Use title-theme for main menu

--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,15 @@
 const state = @import("state-machine.zig");
 const mainmenu = @import("screens/main-menu.zig");
+const titletheme = @import("music/title-theme.zig");
 
 var game_state: state.State = state.State.IN_MENU;
 
 export fn update() void {
     switch (game_state) {
-        .IN_MENU => mainmenu.drawMenu(),
+        .IN_MENU => {
+            mainmenu.drawMenu();
+            titletheme.mainMenuMusic();
+        },
         else => {}
     }
 }
