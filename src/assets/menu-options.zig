28ffb9f96eae99368a1488b180d284a38d5b5a47
Fergus Baker <fergusbkr@gmail.com>
2022-01-29 19:35:52 +0000

    Revert "menu implemented  (#54)" (#58)
    This reverts commit 7914aaea44aeaee51399918aabab8c520e692d6c.

--- src/assets/menu-options.zig
+++
@@ -1,5 +0,0 @@
-const Option = @import("../components/option.zig").Option;
-
-pub const menu_options = [_]Option{
-    Option{ .menu = "Party/nLines", .options = [3][]const u8{ "start","Tutorialial", "Credits" }}
-};
