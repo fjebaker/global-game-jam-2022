e7636a284539ff929e78e6ca5d37b293112690ca
OneSlightWeirdo <www.aereregion.webs.com@live.co.uk>
2022-01-29 15:16:05 +0000

    Add drum and bass lines

--- src/music/title-theme.zig
+++ src/music/title-theme.zig
@@ -3,14 +3,24 @@ const tone = @import("tone.zig");
 const tune = @import("tune.zig");
 const note = @import("note.zig");
 
+const c4 = note.getFreq("C 4".*);
 const c5 = note.getFreq("C 5".*);
 const g4 = note.getFreq("G 4".*);
 const a4 = note.getFreq("A 4".*);
 const d4 = note.getFreq("D 4".*);
 const e4 = note.getFreq("E 4".*);
+const e3 = note.getFreq("E 3".*);
+const eb3 = note.getFreq("Eb3".*);
+const g3 = note.getFreq("G 3".*);
+const gb3 = note.getFreq("Gb3".*);
+const c3 = note.getFreq("C 3".*);
+const f3 = note.getFreq("F 3".*);
 
-var mTone = tone.Tone{ .sfreq = c5, .efreq = c5 };
-var mNotes = [_]note.Note{
+var melodyTone = tone.Tone{ .sfreq = c5, .efreq = c5, .channel = 1 };
+var drumTone = tone.Tone{ .sfreq = c5, .efreq = c5, .channel = 3, .release = 60, .volume = 25, .mode = 2 };
+var bassTone = tone.Tone{ .sfreq = c5, .efreq = c5, .channel = 2 };
+
+var melodyNotes = [_]note.Note{
     note.Note{ .sfreq = c5, .efreq = c5, .length = 60 },
     note.Note{ .sfreq = g4, .efreq = g4, .length = 60 },
     note.Note{ .sfreq = a4, .efreq = a4, .length = 60 },
@@ -19,8 +29,34 @@ var mNotes = [_]note.Note{
     note.Note{ .sfreq = 307, .efreq = 287, .length = 120 },
     note.Note{ .sfreq = 294, .efreq = 255, .length = 240 },
 };
-var mTune = tune.Tune{ .notes = &mNotes, .numNotes = mNotes.len, .tone = &mTone };
+var drumNotes = [_]note.Note{
+    note.Note{ .sfreq = e3, .efreq = e3, .length = 60 },
+    note.Note{ .sfreq = g3, .efreq = g3, .length = 60 },
+    note.Note{ .sfreq = eb3, .efreq = eb3, .length = 60 },
+    note.Note{ .sfreq = gb3, .efreq = g3, .length = 60 },
+    note.Note{ .sfreq = e3, .efreq = e3, .length = 60 },
+    note.Note{ .sfreq = g3, .efreq = g3, .length = 60 },
+    note.Note{ .sfreq = eb3, .efreq = eb3, .length = 60 },
+    note.Note{ .sfreq = gb3, .efreq = g3, .length = 60 },
+    note.Note{ .sfreq = e3, .efreq = e3, .length = 60 },
+    note.Note{ .sfreq = 1, .efreq = 1, .length = 60 },
+    note.Note{ .sfreq = 1, .efreq = 1, .length = 60 },
+    note.Note{ .sfreq = 1, .efreq = 1, .length = 60 },
+};
+var bassNotes = [_]note.Note{
+    note.Note{ .sfreq = c3, .efreq = c3, .length = 120 },
+    note.Note{ .sfreq = e3, .efreq = e3, .length = 120 },
+    note.Note{ .sfreq = g3, .efreq = g3, .length = 120 },
+    note.Note{ .sfreq = gb3, .efreq = gb3, .length = 120 },
+    note.Note{ .sfreq = f3, .efreq = g3, .length = 240 },
+};
+
+var melody = tune.Tune{ .notes = &melodyNotes, .numNotes = melodyNotes.len, .tone = &melodyTone };
+var drums = tune.Tune{ .notes = &drumNotes, .numNotes = drumNotes.len, .tone = &drumTone };
+var bass = tune.Tune{ .notes = &bassNotes, .numNotes = bassNotes.len, .tone = &bassTone };
 
 pub fn mainMenuMusic() void {
-    mTune.play();
+    melody.play();
+    drums.play();
+    bass.play();
 }
