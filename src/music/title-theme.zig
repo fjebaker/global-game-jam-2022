78729a5cf1baf8ed3b7102cc5216c5ce088c0bd6
OneSlightWeirdo <www.aereregion.webs.com@live.co.uk>
2022-01-29 11:54:24 +0000

    Use getfreq in title theme

--- src/music/title-theme.zig
+++ src/music/title-theme.zig
@@ -3,14 +3,20 @@ const tone = @import("tone.zig");
 const tune = @import("tune.zig");
 const note = @import("note.zig");
 
-var mTone = tone.Tone{ .sfreq = 523, .efreq = 523 };
+const c5 = note.getFreq("C 5".*);
+const g4 = note.getFreq("G 4".*);
+const a4 = note.getFreq("A 4".*);
+const d4 = note.getFreq("D 4".*);
+const e4 = note.getFreq("E 4".*);
+
+var mTone = tone.Tone{ .sfreq = c5, .efreq = c5 };
 var mNotes = [_]note.Note{
-    note.Note{ .sfreq = 523, .efreq = 523, .length = 60 },
-    note.Note{ .sfreq = 392, .efreq = 392, .length = 60 },
-    note.Note{ .sfreq = 440, .efreq = 440, .length = 60 },
-    note.Note{ .sfreq = 294, .efreq = 294, .length = 60 },
-    note.Note{ .sfreq = 330, .efreq = 330, .length = 120 },
-    note.Note{ .sfreq = 307, .efreq = 287, .length = 60 },
+    note.Note{ .sfreq = c5, .efreq = c5, .length = 60 },
+    note.Note{ .sfreq = g4, .efreq = g4, .length = 60 },
+    note.Note{ .sfreq = a4, .efreq = a4, .length = 60 },
+    note.Note{ .sfreq = d4, .efreq = d4, .length = 60 },
+    note.Note{ .sfreq = e4, .efreq = e4, .length = 120 },
+    note.Note{ .sfreq = 307, .efreq = 287, .length = 120 },
     note.Note{ .sfreq = 294, .efreq = 255, .length = 240 },
 };
 var mTune = tune.Tune{ .notes = &mNotes, .numNotes = mNotes.len, .tone = &mTone };
