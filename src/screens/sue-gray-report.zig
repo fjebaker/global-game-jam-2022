f52b9f483a589105a01d49c2b7a0519f43115db7
fjebaker <fergusbkr@gmail.com>
2022-01-30 16:06:45 +0000

    zig formatting

--- src/screens/sue-gray-report.zig
+++ src/screens/sue-gray-report.zig
@@ -1,9 +1,15 @@
 const w4 = @import("../wasm4.zig");
 const gamepad = @import("../gamepad.zig");
 const statemachine = @import("../state-machine.zig");
+const std = @import("std");
 
 const SCREEN_SIZE: u8 = 160;
 const TIMEOUT = 60;
+
+fn toString(score: u32, buf: []u8) void {
+    // fixed size buffer of 4
+    _ = std.fmt.bufPrint(buf, "{d}", .{score}) catch {};
+}
 
 pub const SueGrayReport = struct {
     ticker: u32 = 0,
@@ -13,6 +19,11 @@ pub const SueGrayReport = struct {
     }
 
     pub fn update(self: *@This(), state: *statemachine.StateMachine, pl: *gamepad.GamePad) void {
+        w4.DRAW_COLORS.* = 0x42;  
+        var buf: [4]u8 = undefined;
+        toString(state.confidence, &buf);
+        w4.text(&buf, 15, 105);
+        
         if (self.ticker > TIMEOUT) {
             self.handleInput(state, pl);
 
@@ -25,16 +36,17 @@ pub const SueGrayReport = struct {
         self.draw(state);
     }
 
-    fn draw(_: *const @This(), _: *const statemachine.StateMachine) void {
+    fn draw(_: *const @This(), state: *const statemachine.StateMachine) void {
+        w4.DRAW_COLORS.* = 0x42;
+        var buf: [4]u8 = undefined;
+        toString(state.buzzing, &buf);
+        w4.text(&buf, 15, 65);
+
         w4.DRAW_COLORS.* = 0x02;
         w4.text("SUE GRAY REPORT", 20, 5);
         w4.DRAW_COLORS.* = 0x03;
         w4.text("On the 5th of Jan,\nBohnson was found\npartying in 10\nDowning Street. He\nwas:", 5, 20);
         w4.text("Now has a Vote of\nConfidence of", 5, 80);
-
-        w4.DRAW_COLORS.* = 0x42;
-        w4.text("100", 15, 65);
-        w4.text("100", 15, 105);
 
         w4.DRAW_COLORS.* = 0x02;
         w4.text("%Feckless", 40, 65);
