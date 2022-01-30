c43a4a1e747d116f5805e173d029a090db6bc6c0
OneSlightWeirdo <www.aereregion.webs.com@live.co.uk>
2022-01-30 12:06:12 +0000

    Add party vibez music to the main screen

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,7 @@ const w4 = @import("wasm4.zig");
 const statemachine = @import("state-machine.zig");
 const mainmenu = @import("screens/main-menu.zig");
 const titletheme = @import("music/title-theme.zig");
+const partyvibez = @import("music/party-vibez.zig");
 const party = @import("screens/party.zig");
 const startscreen = @import("screens/start-screen.zig");
 const gamepad = @import("gamepad.zig");
@@ -44,6 +45,9 @@ export fn update() void {
         .START_SCREEN => startscreen.update(),
         else => {},
     }
-    titletheme.mainMenuMusic();
+    switch (state.screen) {
+        .AT_PARTY => partyvibez.partyVibezMusic(),
+        else => titletheme.mainMenuMusic(),
+    }
     player.update();
 }
