5ec037917cd2c186807f8fe4a093323757b485b3
fjebaker <fergusbkr@gmail.com>
2022-01-30 01:39:38 +0000

    gavels but not very random

---
+++ src/components/gavel.zig
@@ -0,0 +1,37 @@
+const w4 = @import("../wasm4.zig");
+const sprites = @import("../assets/sprites.zig");
+const projectile = @import("projectile.zig");
+const std = @import("std");
+
+const Projectile = projectile.Projectile;
+const Facing = projectile.Facing;
+
+const SCREEN_SIZE: u8 = 160;
+
+pub fn randomGavel(rnd: *std.rand.Random) Projectile {
+    // select a random edge
+    const edge = rnd.intRangeLessThan(u8, 0, 4);
+    // 0 is left, 1 is top, 2 is right, 3 is bottom
+
+    // select a random starting location
+    const pos = rnd.intRangeLessThan(u8, 0, SCREEN_SIZE);
+    
+    return switch (edge) {
+        0 => Projectile.init(
+            sprites.flag,
+            0, pos, .RIGHT
+        ),
+        1 => Projectile.init(
+            sprites.flag,
+            pos, 0, .DOWN
+        ),
+        2 => Projectile.init(
+            sprites.flag,
+            0, pos, .RIGHT
+        ),
+        else => Projectile.init(
+            sprites.flag,
+            pos, 0, .UP
+        )
+    };
+}
